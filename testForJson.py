3ab144b137c69df9c32090531babc11878124302
jiaqiluo <jiaqiluo123+GitHub@gmail.com>
2017-05-14 12:11:58 -0700

    (Unstable) Changes in the diesgin of JSON file, and sample1

--- testForJson.py
+++ testForJson.py
@@ -4,9 +4,7 @@ from pprint import pprint
 with open('sample1.json') as data_file:
     data = json.load(data_file)
 
-# pprint(data)
-pprint(data["dataflow"])
+pprint(data)
 print("\n")
 pprint(data["layers"])
 print("\n")
-pprint(data["dataflow"]["D"])
